7c952fc5f2c847e89677ed879ecde8918abbec75
rimm <rimm@ooof1.ru>
2020-01-13 10:37:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict'
+let arr=['plum.png','orange.png','aple.png'];
+console.log(arr[0]);
+//alert ("Ура заработало!!");
+//let answer=confirm("Ты здесь?");
+//console.log(answer);`
+let answer=prompt("Сколько тебе лет?",'');
+console.log(typeof(arr));
+console.log(typeof(null));
+console.log("arr"+"sadlkjel po");
